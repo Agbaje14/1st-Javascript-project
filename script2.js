6386cd42c320a87f3746bc43fac29a018b6e6eb5
azeez <115058939+Agbaje14@users.noreply.github.com>
2025-08-24 22:29:50 -0400

    implemented score tracking

--- script2.js
+++ script2.js
@@ -16,7 +16,7 @@ console.log(getComputerChoice());
 
 alert("Welcome to the Rock, Paper, Scissors Game!");
 
-alert("Round 1")
+
 
 function getHumanChoice(){
     let userInput = prompt("Enter your choice(Rock, Paper or Scissors))")
@@ -30,8 +30,6 @@ console.log (getHumanChoice());
 
 function playgame(){
 
-    const humanSelection = getComputerChoice();
-    const computerSelection = getComputerChoice();
 
     let humanScore = 0
     let computerScore = 0
@@ -44,39 +42,63 @@ function playgame(){
         if (humanAns === compAns){
             alert(`You and the computer both chose ${compAns} so its a tie!`)
         } else if (
-            (humanAns === "rock" && compAns ==="scissors") || (humanAns === "scissors" && compAns === "paper") || (humanAns === "paper" & compAns === "rock")
+            (humanAns === "rock" && compAns ==="scissors") || (humanAns === "scissors" && compAns === "paper") || (humanAns === "paper" && compAns === "rock")
         ){
             humanScore++;
             alert (`You win because ${humanAns} beats ${compAns}`)
         } else {
             alert (`You lose because ${compAns} beats ${humanAns}`)
+            computerScore++;
         }
 
         alert(`Your score: ${humanScore}  computer score: ${computerScore}`)
 
     }
 
+    function round1(){
+    alert ("Round 1!")
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
+    }
+
     function round2(){
         alert ("Round 2!")
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
     }
+
     function round3(){
         alert ("Round 3!")
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
     }
     function round4(){
         alert ("Round 4!")
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
     }
     function round5(){
         alert ("Round 5!")
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
     }
 
+    round1();
     round2();
     round3();
     round4();
     round5();
+
+    if (humanScore > computerScore){
+        alert("You win the game!")
+    } else {
+        alert("The computer wins")
+    }
 
     
     // playRound(humanSelection, computerSelection);
